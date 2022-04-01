a783ace5e2880235455c1b388c3e70ad6ea80e69
Joanna <joanna.tr48@gmail.com>
2022-04-01 13:06:57 +0200

    2 Probleme gelöst: Problem: Call requires permission which may be rejected by user Problem:  fusedLocationClient.requestLocationUpdates(getLocationRequest(),          				locationCallback, null)

--- app/src/main/java/com/example/mysimplesensorapp/LocationActivity.kt
+++ app/src/main/java/com/example/mysimplesensorapp/LocationActivity.kt
@@ -1,8 +1,12 @@
 package com.example.mysimplesensorapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
 
 
@@ -19,7 +23,24 @@ class LocationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location)
 
-      fusedLocationClient.lastLocation.addOnSuccessListener {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener {
                 if(it != null){
                     tvLocation.text = getString(R.string.locationTVtxt, it.latitude, it.longitude, it.time)
                 }
@@ -37,7 +58,8 @@ class LocationActivity : AppCompatActivity() {
             }
         }
 
-        fusedLocationClient.requestLocationUpdates(getLocationRequest(), locationCallback, null)
+        fusedLocationClient.requestLocationUpdates(getLocationRequest(),
+            locationCallback, Looper.getMainLooper())
 
     }
 
