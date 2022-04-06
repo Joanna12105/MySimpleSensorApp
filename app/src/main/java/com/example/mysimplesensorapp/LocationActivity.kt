70b3780e9b90e25b7be79b62a259b3602b56d75d
Joanna <joanna.tr48@gmail.com>
2022-04-06 13:00:42 +0200

    LocationActivity gibt keine Daten aus - LOG eingefügt

--- app/src/main/java/com/example/mysimplesensorapp/LocationActivity.kt
+++ app/src/main/java/com/example/mysimplesensorapp/LocationActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
+import android.util.Log
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
@@ -23,43 +24,32 @@ class LocationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location)
 
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
+        Log.i(TAG, "onCreate")
+
         fusedLocationClient.lastLocation.addOnSuccessListener {
-                if(it != null){
-                    tvLocation.text = getString(R.string.locationTVtxt, it.latitude, it.longitude, it.time)
-                }
-                else {
-                    tvLocation.text = getString(R.string.noAvailablePosition)
-                }
+            if(it != null){
+                tvLocation.text = getString(R.string.location_value, it.latitude, it.longitude, it.time)
+                Log.i(TAG, "location available")
             }
+            else{
+                tvLocation.text = getString(R.string.no_available_position)
+                Log.i(TAG, "location not available")
+
+            }
+        }
 
         val locationCallback = object :  LocationCallback() {
             override fun onLocationResult(locationResult : LocationResult) {
                 val location = locationResult.lastLocation
+                Log.i(TAG, "onLocationResult " + location.toString())
                 if(location != null){
-                    tvLocation.text = getString(R.string.locationTVtxt, location.latitude, location.longitude, location.time)
+                    tvLocation.text = getString(R.string.location_value, location.latitude, location.longitude, location.time)
                 }
             }
         }
 
-        fusedLocationClient.requestLocationUpdates(getLocationRequest(),
-            locationCallback, Looper.getMainLooper())
+        fusedLocationClient.requestLocationUpdates(getLocationRequest(), locationCallback, null)
+
 
     }
 
